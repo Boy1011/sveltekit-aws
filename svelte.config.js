78d65da64cab46a80e0c7ddbbc9d4cdd1718ac27
BOY <boy@BOYs-MacBook-Pro.local>
2022-12-30 11:18:36 +0700

    pull test

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+
+///** @type {import('@sveltejs/kit').Config} */
+//const config = {
+//	kit: {
+//		adapter: adapter()
+//	}
+//};
+
+//export default config;
+
+//awd amplify adapter
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      default: true,
+    },
+  },
 };
 
 export default config;
